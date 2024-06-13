25641d791db32a37c31531ead76e9aca30e8b891
CyberFinn <stephendiavolo@gmail.com>
2024-06-13 18:30:13 +0200

    Fix: Reworked logic Description: The caesar cipher now caters for spaces in words - allowing for longer ciphertexts, and operates faster than before.

--- src/caesarcipher/frmMain.java
+++ src/caesarcipher/frmMain.java
@@ -6,73 +6,22 @@ package caesarcipher;
 
 /**
  *
- * @author CP352626
+ * @author CyberFinn
  */
-public class frmMain extends javax.swing.JFrame 
+public class frmMain extends javax.swing.JFrame
 {
-    private static String strCipherCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890~'!@#$%^&*()[]{}:?/";
-    private int intKey = 11;
+
+    private final char[] CaesarRow1 = {'a','b','c','d','e','f','g','h','i','j','k','l','m'};
+    private final char[] CaesarRow2 = {'n','o','p','q','r','s','t','u','v','w','x','y','z'};
+    private String userInputString = "";
+    private String DecryptedString = "";
+    
     /**
      * Creates new form frmMain
      */
-    public frmMain() {
+    public frmMain()
+    {
         initComponents();
-    }
-    private String mCleanString(String strText)
-    {
-        strText = strText.replaceAll("\n", "");
-        
-        for (int x = 0; x < strText.length(); x++) 
-        {
-            int intPos = strCipherCharacters.indexOf(strText.charAt(x));
-            if(intPos == -1)
-            {
-                strText = strText.replace(strText.charAt(x), ' ');
-            }
-        }
-        return strText;
-    }
-    
-    public String decryptString(String strText, int intKey)
-    {
-        String strCleanedText = mCleanString(strText);
-        String strDecrypted = "";
-        
-        for (int i = 0; i < strCleanedText.length(); i++) 
-        {
-            int intCharPos = strCipherCharacters.indexOf(strCleanedText.charAt(i)); 
-            
-            if((intCharPos - intKey)<0)
-            {
-                strDecrypted += strCipherCharacters.charAt((intCharPos-intKey)+strCipherCharacters.length());
-            }
-            else
-            {
-                strDecrypted += strCipherCharacters.charAt(intCharPos-intKey);
-            }
-        }
-        return strDecrypted;
-    }
-    
-    public String encryptString(String strText, int intKey)
-    {
-        String strCleanedText = mCleanString(strText);
-        String strEncrypted = "";
-        
-        for (int i = 0; i < strCleanedText.length(); i++) 
-        {
-            int intCharPos = strCipherCharacters.indexOf(strCleanedText.charAt(i)); 
-            
-            if((intCharPos + intKey)<(strCipherCharacters.length()))
-            {
-                strEncrypted += strCipherCharacters.charAt(intCharPos+intKey);
-            }
-            else
-            {
-                strEncrypted += strCipherCharacters.charAt((intCharPos+intKey)-strCipherCharacters.length());
-            }
-        }
-        return strEncrypted;
     }
 
     /**
@@ -85,11 +34,12 @@ public class frmMain extends javax.swing.JFrame
     private void initComponents() {
 
         btnDecrypt = new javax.swing.JButton();
+        txtInputText = new javax.swing.JTextField();
+        txtOutputTxt = new javax.swing.JTextField();
         btnEncrypt = new javax.swing.JButton();
-        txtEncrypt = new javax.swing.JTextField();
-        txtDecrypt = new javax.swing.JTextField();
-        txtValue = new javax.swing.JTextField();
-        jLabel1 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        txtResult = new javax.swing.JTextField();
+        btnClearOutput = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -100,104 +50,231 @@ public class frmMain extends javax.swing.JFrame
             }
         });
 
+        txtOutputTxt.setEditable(false);
+
         btnEncrypt.setText("Encrypt");
+        btnEncrypt.setName("btnEncrypt"); // NOI18N
         btnEncrypt.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnEncryptActionPerformed(evt);
             }
         });
 
-        jLabel1.setText("Decrypted:");
+        jLabel3.setText("Result:");
+
+        btnClearOutput.setText("Clear");
+        btnClearOutput.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnClearOutputActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(btnEncrypt, javax.swing.GroupLayout.Alignment.TRAILING)
                     .addComponent(btnDecrypt, javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jLabel1, javax.swing.GroupLayout.Alignment.TRAILING))
+                    .addComponent(btnEncrypt, javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jLabel3, javax.swing.GroupLayout.Alignment.TRAILING))
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(txtEncrypt, javax.swing.GroupLayout.DEFAULT_SIZE, 220, Short.MAX_VALUE)
-                    .addComponent(txtDecrypt)
-                    .addComponent(txtValue, javax.swing.GroupLayout.Alignment.TRAILING))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(txtOutputTxt)
+                    .addComponent(txtInputText)
+                    .addComponent(txtResult, javax.swing.GroupLayout.PREFERRED_SIZE, 319, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addComponent(btnClearOutput)
+                .addContainerGap(39, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(35, 35, 35)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(btnEncrypt)
-                    .addComponent(txtEncrypt, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                    .addComponent(txtInputText, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnEncrypt))
+                .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtOutputTxt, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(btnDecrypt)
-                    .addComponent(txtDecrypt, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                    .addComponent(btnClearOutput))
+                .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(txtValue, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel1))
+                    .addComponent(jLabel3)
+                    .addComponent(txtResult, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap(29, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void btnEncryptActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEncryptActionPerformed
+    private void btnDecryptActionPerformed(java.awt.event.ActionEvent evt)//GEN-FIRST:event_btnDecryptActionPerformed
+    {//GEN-HEADEREND:event_btnDecryptActionPerformed
         // TODO add your handling code here:
-        txtDecrypt.setText(encryptString(txtEncrypt.getText(), intKey));
-    }//GEN-LAST:event_btnEncryptActionPerformed
-
-    private void btnDecryptActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDecryptActionPerformed
-        // TODO add your handling code here:
-        txtValue.setText(decryptString(txtDecrypt.getText(), intKey));
+        if(loadUpUserInputsDecrypt())
+        {
+            DecryptedString = "";
+            DecryptUserInput();
+            
+            txtResult.setText("");
+            txtResult.setText(DecryptedString);
+        }
     }//GEN-LAST:event_btnDecryptActionPerformed
 
+    private void btnEncryptActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEncryptActionPerformed
+        // TODO add your handling code here:
+        if(loadUpUserInputs())
+        {
+            DecryptUserInput();
+            
+            txtOutputTxt.setText("");
+            txtOutputTxt.setText(DecryptedString);
+        }
+    }//GEN-LAST:event_btnEncryptActionPerformed
+
+    private void btnClearOutputActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnClearOutputActionPerformed
+        // TODO add your handling code here:
+        txtOutputTxt.setText("");
+        DecryptedString = "";
+    }//GEN-LAST:event_btnClearOutputActionPerformed
+
+    private Boolean loadUpUserInputs()
+    {
+        Boolean returnedVal = true;
+        if(!txtInputText.getText().equals(""))
+        {
+            userInputString = txtInputText.getText().toLowerCase();
+        } else returnedVal = false;
+        
+        return returnedVal;
+    }
+    
+    private Boolean loadUpUserInputsDecrypt()
+    {
+        Boolean returnedVal = true;
+        if(!txtOutputTxt.getText().equals(""))
+        {
+            userInputString = txtOutputTxt.getText().toLowerCase();
+        } else returnedVal = false;
+        
+        return returnedVal;
+    }
+    
+    private void DecryptUserInput()
+    {
+        for(char c : userInputString.toCharArray())
+        {
+            if(c == ' ')
+                DecryptedString += c+"";
+            else
+                determinePosInArray(c);
+        }
+    }
+    
+    private void determinePosInArray(char c)
+    {
+        char arrayThatHasData = '0'; //0 = default, 1 = arr1, 2 = arr2
+        int positionInArray = 0;
+        int array1Index = 0;
+        int array2Index = 0;
+        
+        for(char a : CaesarRow1)
+        {
+            if(a==c)
+            {
+                arrayThatHasData = '1';
+                positionInArray = array1Index;
+                break;
+            }
+            array1Index++;
+        }
+        
+        if(arrayThatHasData=='1')
+        {
+            getItemInCaesarArray2AtIndex(positionInArray);
+            return;
+        }
+        
+        for(char a : CaesarRow2)
+        {
+            if(a==c)
+            {
+                arrayThatHasData = '2';
+                positionInArray = array2Index;
+                break;
+            }
+            array2Index++;
+        }
+        
+        if(arrayThatHasData=='2')
+        {
+            getItemInCaesarArray1AtIndex(positionInArray);
+        }
+    }
+    
+    private void getItemInCaesarArray1AtIndex(int index)
+    {
+        DecryptedString += CaesarRow1[index]; 
+    }
+    private void getItemInCaesarArray2AtIndex(int index)
+    {
+        DecryptedString += CaesarRow2[index];    
+    }
+    
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
+    public static void main(String args[])
+    {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
          */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
+        try
+        {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels())
+            {
+                if ("Nimbus".equals(info.getName()))
+                {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }
             }
-        } catch (ClassNotFoundException ex) {
+        } catch (ClassNotFoundException ex)
+        {
             java.util.logging.Logger.getLogger(frmMain.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
+        } catch (InstantiationException ex)
+        {
             java.util.logging.Logger.getLogger(frmMain.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
+        } catch (IllegalAccessException ex)
+        {
             java.util.logging.Logger.getLogger(frmMain.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+        } catch (javax.swing.UnsupportedLookAndFeelException ex)
+        {
             java.util.logging.Logger.getLogger(frmMain.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
+        java.awt.EventQueue.invokeLater(new Runnable()
+        {
+            public void run()
+            {
                 new frmMain().setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnClearOutput;
     private javax.swing.JButton btnDecrypt;
     private javax.swing.JButton btnEncrypt;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JTextField txtDecrypt;
-    private javax.swing.JTextField txtEncrypt;
-    private javax.swing.JTextField txtValue;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JTextField txtInputText;
+    private javax.swing.JTextField txtOutputTxt;
+    private javax.swing.JTextField txtResult;
     // End of variables declaration//GEN-END:variables
 }
